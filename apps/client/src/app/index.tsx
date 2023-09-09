a4abac5e7313e8d441ccbcdbebbcc01a9b14cbd4
stvncode <steven.travers20@gmail.com>
2023-09-10 00:29:09 +0200

    Init turbo repo

---
+++ apps/client/src/app/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./styles.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Admin <br />
+        <span>Kitchen Sink</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
